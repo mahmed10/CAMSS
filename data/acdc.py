2c052357313afddef05c84352d8a1b99dc376757
Masud Ahmed <60801849+mahmed10@users.noreply.github.com>
2025-05-01 15:05:53 -0400

    Update acdc.py

--- data/acdc.py
+++ data/acdc.py
@@ -3,7 +3,7 @@ from PIL import Image
 from data.base_dataset import BaseDataset
 
 class ACDC(BaseDataset):
-    def __init__(self, path_list, config_file = "./dataset/CityScapes/cityscape_copy.yaml", transform = None,
+    def __init__(self, path_list, config_file = "./dataset/ACDC/acdc.yaml", transform = None,
                 data_set = 'val', seed=None, img_size=768, interpolation=Image.BILINEAR, color_pallete = 'city'):
         super().__init__(path_list, transform, data_set, seed, img_size, interpolation, color_pallete)
         with open(config_file, 'r') as stream:
